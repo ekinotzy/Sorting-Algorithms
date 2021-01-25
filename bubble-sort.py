c401eb612156ed3d4531098517b7d9ec44e1c678
ekinotzy <77970558+ekinotzy@users.noreply.github.com>
2021-01-25 09:41:29 -0300

    Update bubble-sort.py

--- bubble-sort.py
+++ bubble-sort.py
@@ -10,8 +10,7 @@ def bubbleSort(arr):
         for j in range(0, n-i-1):
         
             # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
+            # Swap if the element found is greater than the next element
             if arr[j] > arr[j+1]
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 
